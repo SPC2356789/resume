55fc2e68003415e20f4119fb1e57f674d7c18f78
嶸 <101851321+SPC2356789@users.noreply.github.com>
2025-03-21 01:54:49 +0800

    no message

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,10 +18,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-//    public function boot()
-//    {
-//        if (env('APP_ENV') === 'local') {
-//            URL::forceScheme('https');
-//        }
-//    }
+    public function boot()
+    {
+        if (env('APP_ENV') === 'local') {
+            URL::forceScheme('https');
+        }
+    }
 }
