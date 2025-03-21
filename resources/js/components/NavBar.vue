b6bc350e931859f8ee7ff17ef2f4525488f4be56
嶸 <101851321+SPC2356789@users.noreply.github.com>
2025-03-21 09:03:11 +0800

    no message

--- resources/js/components/NavBar.vue
+++ resources/js/components/NavBar.vue
@@ -6,7 +6,7 @@
                     v-for="item in items"
                     :key="item.name"
                     :title="item.name"
-                    :href="`${item.base??'/'}${item.link}`"
+                    :href="`${item.base??'/vue/'}${item.link}`"
                 />
             </v-list>
         </v-navigation-drawer>
